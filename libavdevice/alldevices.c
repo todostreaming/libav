232d7ac544aec735cc629f50250f97881b043d13
Luca Barbato <lu_zero@gentoo.org>
2013-02-19 23:23:35 +0100

    avdevice: Add Blackmagic Design Decklink capture

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -49,8 +49,10 @@ void avdevice_register_all(void)
     /* devices */
     REGISTER_INOUTDEV(ALSA,             alsa);
     REGISTER_INDEV   (BKTR,             bktr);
+    REGISTER_INDEV   (BMD,              bmd);
     REGISTER_INDEV   (DV1394,           dv1394);
     REGISTER_INDEV   (FBDEV,            fbdev);
+    REGISTER_INDEV   (JACK,             jack);
     REGISTER_INDEV   (JACK,             jack);
     REGISTER_INOUTDEV(OSS,              oss);
     REGISTER_INDEV   (PULSE,            pulse);
