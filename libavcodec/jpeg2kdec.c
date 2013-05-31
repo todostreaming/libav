2312ec14381d148c65ae116840c1540cb1564327
Michael Niedermayer <michaelni@gmx.at>
2013-05-31 15:36:49 +0200

    jpeg2k: optimize dequantization
    Float:   4700 -> 2700 cycles
    Integer: 4400 -> 2800 cycles
    
    (sandybridge  i7)

--- libavcodec/jpeg2kdec.c
+++ libavcodec/jpeg2kdec.c
@@ -986,13 +986,14 @@ static void dequantization_float(int x, int y, Jpeg2KCblk *cblk,
                                  Jpeg2KComponent *comp,
                                  Jpeg2KT1Context *t1, Jpeg2KBand *band)
 {
-    int i, j, idx;
-    float *datap = &comp->f_data[(comp->coord[0][1] - comp->coord[0][0]) * y + x];
-    for (j = 0; j < (cblk->coord[1][1] - cblk->coord[1][0]); ++j)
-        for (i = 0; i < (cblk->coord[0][1] - cblk->coord[0][0]); ++i) {
-            idx        = (comp->coord[0][1] - comp->coord[0][0]) * j + i;
-            datap[idx] = (float)(t1->data[j][i]) * band->f_stepsize;
-        }
+    int i, j;
+    int w = cblk->coord[0][1] - cblk->coord[0][0];
+    for (j = 0; j < (cblk->coord[1][1] - cblk->coord[1][0]); ++j) {
+        float *datap = &comp->f_data[(comp->coord[0][1] - comp->coord[0][0]) * (y + j) + x];
+        int *src = t1->data[j];
+        for (i = 0; i < w; ++i)
+            datap[i] = src[i] * band->f_stepsize;
+    }
 }
 
 /* Integer dequantization of a codeblock.*/
@@ -1000,14 +1001,14 @@ static void dequantization_int(int x, int y, Jpeg2KCblk *cblk,
                                Jpeg2KComponent *comp,
                                Jpeg2KT1Context *t1, Jpeg2KBand *band)
 {
-    int i, j, idx;
-    int32_t *datap = &comp->i_data[(comp->coord[0][1] - comp->coord[0][0]) * y + x];
-    for (j = 0; j < (cblk->coord[1][1] - cblk->coord[1][0]); ++j)
-        for (i = 0; i < (cblk->coord[0][1] - cblk->coord[0][0]); ++i) {
-            idx        = (comp->coord[0][1] - comp->coord[0][0]) * j + i;
-            datap[idx] =
-                ((int32_t)(t1->data[j][i]) * band->i_stepsize + (1 << 15)) >> 16;
-        }
+    int i, j;
+    int w = cblk->coord[0][1] - cblk->coord[0][0];
+    for (j = 0; j < (cblk->coord[1][1] - cblk->coord[1][0]); ++j) {
+        int32_t *datap = &comp->i_data[(comp->coord[0][1] - comp->coord[0][0]) * (y + j) + x];
+        int *src = t1->data[j];
+        for (i = 0; i < w; ++i)
+            datap[i] = (src[i] * band->i_stepsize + (1 << 15)) >> 16;
+    }
 }
 
 /* Inverse ICT parameters in float and integer.
