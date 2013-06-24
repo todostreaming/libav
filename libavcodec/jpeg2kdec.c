99641f0de2a15df38b29d55dc66a384a3fb54c47
Michael Niedermayer <michaelni@gmx.at>
2013-06-24 16:26:14 +0200

    jpeg2k: Do not crash on NULL node in tag_tree_decode
    Signed-off-by: Luca Barbato <lu_zero@gentoo.org>

--- libavcodec/jpeg2kdec.c
+++ libavcodec/jpeg2kdec.c
@@ -119,6 +119,9 @@ static int tag_tree_decode(Jpeg2KDecoderContext *s, Jpeg2KTgtNode *node,
     Jpeg2KTgtNode *stack[30];
     int sp = -1, curval = 0;
 
+    if (!node)
+        return AVERROR_INVALIDDATA;
+
     while (node && !node->vis) {
         stack[++sp] = node;
         node        = node->parent;
