e77ad3e28e755c8a94809823361dfef009f6579f
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-02-17 14:36:28 +0100

    mvc: remove 15 and 20 from parser

--- libavcodec/h264_parser.c
+++ libavcodec/h264_parser.c
@@ -60,13 +60,13 @@ static int h264_find_frame_end(H264Context *h, const uint8_t *buf,
         } else if (state <= 5) {
             int v = buf[i] & 0x1F;
             // any extradata the frame might start with
-            if (v == 6 || v == 7 || v == 8 || v == 9 || v == 14 || v == 15) {
+            if (v == 6 || v == 7 || v == 8 || v == 9 || v == 14) {
                 if (pc->frame_start_found) {
                     i++;
                     goto found;
                 }
             // video slices only
-            } else if (v == 1 || v == 2 || v == 5 || v == 20) { // 20 here is maybe unneeded
+            } else if (v == 1 || v == 2 || v == 5) {
                 if (pc->frame_start_found) {
                     state += 8;
                     continue;
