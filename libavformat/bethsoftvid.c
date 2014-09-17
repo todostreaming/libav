823f5b1423fa13fe1b2e5fb96071f46f42d666a8
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-09-17 16:48:09 +0100

    bethsoftvid: check return value
    CC: libav-stable@libav.org
    Bug-Id: CID 733777

--- libavformat/bethsoftvid.c
+++ libavformat/bethsoftvid.c
@@ -189,6 +189,8 @@ static int read_frame(BVID_DemuxContext *vid, AVIOContext *pb, AVPacket *pkt,
     if (vid->palette) {
         uint8_t *pdata = av_packet_new_side_data(pkt, AV_PKT_DATA_PALETTE,
                                                  BVID_PALETTE_SIZE);
+        if (!pdata)
+            return AVERROR(ENOMEM);
         memcpy(pdata, vid->palette, BVID_PALETTE_SIZE);
         av_freep(&vid->palette);
     }
