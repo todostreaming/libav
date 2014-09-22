23adb0b21278cbb4a56e2b15ab01e3e109455f61
Vittorio Giovara <vittorio.giovara@gmail.com>
2014-09-22 18:02:06 +0100

    rtpenc: check mb_info return value before using it
    CC: libav-stable@libav.org
    Bug-Id: CID 1035715

--- libavformat/rtpenc.c
+++ libavformat/rtpenc.c
@@ -569,8 +569,13 @@ static int rtp_write_packet(AVFormatContext *s1, AVPacket *pkt)
             const uint8_t *mb_info =
                 av_packet_get_side_data(pkt, AV_PKT_DATA_H263_MB_INFO,
                                         &mb_info_size);
-            ff_rtp_send_h263_rfc2190(s1, pkt->data, size, mb_info, mb_info_size);
-            break;
+            if (!mb_info) {
+                av_log(s1, AV_LOG_WARNING, "No MB info found.\n")
+            } else {
+                ff_rtp_send_h263_rfc2190(s1, pkt->data, size,
+                                         mb_info, mb_info_size);
+                break;
+            }
         }
         /* Fallthrough */
     case AV_CODEC_ID_H263P:
