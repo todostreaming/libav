534f9dfe2982144a82f06719bc3159e8a9bd7f1f
Luca Barbato <lu_zero@gentoo.org>
2014-11-11 14:35:37 +0100

    latm: Use ff_combine_packet

--- libavcodec/latm_parser.c
+++ libavcodec/latm_parser.c
@@ -87,18 +87,19 @@ static int latm_parse(AVCodecParserContext *s1, AVCodecContext *avctx,
 {
     LATMParseContext *s = s1->priv_data;
     ParseContext *pc    = &s->pc;
-    int next;
+    int next, ret;
 
     if (s1->flags & PARSER_FLAG_COMPLETE_FRAMES) {
         next = buf_size;
     } else {
         next = latm_find_frame_end(s1, buf, buf_size);
 
-        if (ff_combine_frame(pc, next, &buf, &buf_size) < 0) {
-            *poutbuf      = NULL;
-            *poutbuf_size = 0;
+        ret  = ff_combine_packet(pc, next, &buf, &buf_size,
+                                 poutbuf, poutbuf_size);
+        if (ret == AVERROR(EAGAIN))
             return buf_size;
-        }
+        if (ret == AVERROR(ENOMEM))
+            return ret;
     }
     *poutbuf      = buf;
     *poutbuf_size = buf_size;
