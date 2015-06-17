8be9e70edaac562f9864fa4622eb17a1bce642a8
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-06-17 01:34:37 +0100

    starting with dxt5

--- libavcodec/dxv.c
+++ libavcodec/dxv.c
@@ -38,6 +38,7 @@ typedef struct DXVContext {
 
     uint8_t *tex_data;  // Compressed texture
     int tex_rat;        // Compression ratio
+    int tex_step;       // Distance between blocks
     int tex_size;       // Texture size
 
     /* Pointer to the selected decompression function */
@@ -52,7 +53,7 @@ static int decompress_texture_thread(AVCodecContext *avctx, void *arg,
     int x = (TEXTURE_BLOCK_W * block_nb) % avctx->coded_width;
     int y = TEXTURE_BLOCK_H * (TEXTURE_BLOCK_W * block_nb / avctx->coded_width);
     uint8_t *p = frame->data[0] + x * 4 + y * frame->linesize[0];
-    const uint8_t *d = ctx->tex_data + block_nb * ctx->tex_rat;
+    const uint8_t *d = ctx->tex_data + block_nb * ctx->tex_step;
 
     ctx->tex_funct(p, frame->linesize[0], d);
     return 0;
@@ -153,6 +154,115 @@ static int dxv_decompress_dxt1(AVCodecContext *avctx)
     return 0;
 }
 
+/* DXT5 looks the same except that it works with four
+32-bit elements at once and can have a long copy (i.e. more than one element
+at a time).  */
+
+/* This scheme addresses already decoded elements depending on 2-bit status:
+ *   0 -> copy new element
+ *   1 -> copy one element from position -2
+ *   2 -> copy one element from position -(get_byte() + 2) * 2
+ *   3 -> copy one element from position -(get_16le() + 0x102) * 2 */
+#define DXT1_CHECKPOINT()                                                     \
+    do {                                                                      \
+        if (state == 0) {                                                     \
+            value = bytestream2_get_le32(gbc);                                \
+            state = 16;                                                       \
+        }                                                                     \
+        op = value & 0x3;                                                     \
+        value >>= 2;                                                          \
+        state--;                                                              \
+        switch (op) {                                                         \
+        case 1: /* copy one element from position -2 */                       \
+            idx = 2;                                                          \
+            break;                                                            \
+        case 2: /* copy one element from position -(get_byte() + 2) * 2 */    \
+            idx = (bytestream2_get_byte(gbc) + 2) * 2;                        \
+            break;                                                            \
+        case 3: /* copy one element from position -(get_16le() + 0x102) * 2 */\
+            idx = (bytestream2_get_le16(gbc) + 0x102) * 2;                    \
+            break;                                                            \
+        }                                                                     \
+    } while(0)
+
+static int dxv_decompress_dxt5(AVCodecContext *avctx)
+{
+    DXVContext *ctx = avctx->priv_data;
+    GetByteContext *gbc = &ctx->gbc;
+    uint32_t value, op;
+    int idx, prev, state = 0;
+    int pos = 4;
+
+    /* Copy the first four elements */
+    AV_WL32(ctx->tex_data +  0, bytestream2_get_le32(gbc));
+    AV_WL32(ctx->tex_data +  4, bytestream2_get_le32(gbc));
+    AV_WL32(ctx->tex_data +  8, bytestream2_get_le32(gbc));
+    AV_WL32(ctx->tex_data + 12, bytestream2_get_le32(gbc));
+
+    /* Process input until the whole texture has been filled */
+    while (pos < ctx->tex_size / 4) {
+        DXT1_CHECKPOINT();
+
+        /* Copy two elements from a previous offset or from the input buffer */
+        if (op) {
+            prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+            prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+            prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+            prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+        } else {
+            DXT1_CHECKPOINT();
+
+            if (op)
+                prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            else
+                prev = bytestream2_get_le32(gbc);
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+            DXT1_CHECKPOINT();
+
+            if (op)
+                prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            else
+                prev = bytestream2_get_le32(gbc);
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+            DXT1_CHECKPOINT();
+
+            if (op)
+                prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            else
+                prev = bytestream2_get_le32(gbc);
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+
+            DXT1_CHECKPOINT();
+
+            if (op)
+                prev = AV_RL32(ctx->tex_data + 4 * (pos - idx));
+            else
+                prev = bytestream2_get_le32(gbc);
+            AV_WL32(ctx->tex_data + 4 * pos, prev);
+            pos++;
+        }
+    }
+
+    return 0;
+}
+
 static int dxv_decode(AVCodecContext *avctx, void *data,
                       int *got_frame, AVPacket *avpkt)
 {
@@ -160,23 +270,24 @@ static int dxv_decode(AVCodecContext *avctx, void *data,
     GetByteContext *gbc = &ctx->gbc;
     AVFrame *frame = data;
     uint32_t version, tag;
-    int ret, blocks, size, consumed;
+    int ret, blocks, size;
     int (*decompress_tex)(AVCodecContext *avctx);
 
     bytestream2_init(gbc, avpkt->data, avpkt->size);
-
-    if (bytestream2_get_bytes_left(gbc) < 20) {
-        av_log(avctx, AV_LOG_ERROR, "Frame is too small (%d).",
-               bytestream2_get_bytes_left(gbc));
-        return AVERROR_INVALIDDATA;
-    }
 
     tag = bytestream2_get_le32(gbc);
     switch (tag) {
     case MKBETAG('D', 'X', 'T', '1'):
         ctx->tex_funct = ctx->texdsp.dxt1_block;
         ctx->tex_rat   = 8;
+        ctx->tex_step  = 8;
         decompress_tex = dxv_decompress_dxt1;
+        break;
+    case MKBETAG('D', 'X', 'T', '5'):
+        ctx->tex_funct = ctx->texdsp.dxt5_block;
+        ctx->tex_rat   = 4;
+        ctx->tex_step  = 16;
+        decompress_tex = dxv_decompress_dxt5;
         break;
     default:
         av_log(avctx, AV_LOG_ERROR, "Unsupported tag header (0x%08X).\n", tag);
