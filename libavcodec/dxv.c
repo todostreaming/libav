9e69640a01016655c9802ed56436e51c363ae3eb
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-06-28 16:47:00 +0200

    dxdi works too

--- libavcodec/dxv.c
+++ libavcodec/dxv.c
@@ -45,13 +45,159 @@ typedef struct DXVContext {
     int (*tex_funct)(uint8_t *dst, ptrdiff_t stride, const uint8_t *block);
 } DXVContext;
 
+
+#if USE_REP_MOVSB /* small win on amd, big loss on intel */
+#if (__i386 || __amd64) && __GNUC__ >= 3
+# define lzf_movsb(dst, src, len)                \
+   asm ("rep movsb"                              \
+        : "=D" (dst), "=S" (src), "=c" (len)     \
+        :  "0" (dst),  "1" (src),  "2" (len));
+#endif
+#endif
+
+# include <errno.h>
+# define SET_ERRNO(n) errno = (n)
+# define CHECK_INPUT 1
+
+#define u8 uint8_t
+static unsigned int 
+lzf_decompress (const void *const in_data,  unsigned int in_len,
+                void             *out_data, unsigned int out_len)
+{
+  u8 const *ip = (const u8 *)in_data;
+  u8       *op = (u8 *)out_data;
+  u8 const *const in_end  = ip + in_len;
+  u8       *const out_end = op + out_len;
+
+  do
+    {
+      unsigned int ctrl = *ip++;
+
+      if (ctrl < (1 << 5)) /* literal run */
+        {
+          ctrl++;
+
+          if (op + ctrl > out_end)
+            {
+              SET_ERRNO (E2BIG);
+              return 0;
+            }
+
+#if CHECK_INPUT
+          if (ip + ctrl > in_end)
+            {
+              SET_ERRNO (EINVAL);
+              return 0;
+            }
+#endif
+
+#ifdef lzf_movsb
+          lzf_movsb (op, ip, ctrl);
+#else
+          switch (ctrl)
+            {
+              case 32: *op++ = *ip++; case 31: *op++ = *ip++; case 30: *op++ = *ip++; case 29: *op++ = *ip++;
+              case 28: *op++ = *ip++; case 27: *op++ = *ip++; case 26: *op++ = *ip++; case 25: *op++ = *ip++;
+              case 24: *op++ = *ip++; case 23: *op++ = *ip++; case 22: *op++ = *ip++; case 21: *op++ = *ip++;
+              case 20: *op++ = *ip++; case 19: *op++ = *ip++; case 18: *op++ = *ip++; case 17: *op++ = *ip++;
+              case 16: *op++ = *ip++; case 15: *op++ = *ip++; case 14: *op++ = *ip++; case 13: *op++ = *ip++;
+              case 12: *op++ = *ip++; case 11: *op++ = *ip++; case 10: *op++ = *ip++; case  9: *op++ = *ip++;
+              case  8: *op++ = *ip++; case  7: *op++ = *ip++; case  6: *op++ = *ip++; case  5: *op++ = *ip++;
+              case  4: *op++ = *ip++; case  3: *op++ = *ip++; case  2: *op++ = *ip++; case  1: *op++ = *ip++;
+            }
+#endif
+        }
+      else /* back reference */
+        {
+          unsigned int len = ctrl >> 5;
+
+          u8 *ref = op - ((ctrl & 0x1f) << 8) - 1;
+
+#if CHECK_INPUT
+          if (ip >= in_end)
+            {
+              SET_ERRNO (EINVAL);
+              return 0;
+            }
+#endif
+          if (len == 7)
+            {
+              len += *ip++;
+#if CHECK_INPUT
+              if (ip >= in_end)
+                {
+                  SET_ERRNO (EINVAL);
+                  return 0;
+                }
+#endif
+            }
+
+          ref -= *ip++;
+
+          if (op + len + 2 > out_end)
+            {
+              SET_ERRNO (E2BIG);
+              return 0;
+            }
+
+          if (ref < (u8 *)out_data)
+            {
+              SET_ERRNO (EINVAL);
+              return 0;
+            }
+
+#ifdef lzf_movsb
+          len += 2;
+          lzf_movsb (op, ref, len);
+#else
+          switch (len)
+            {
+              default:
+                len += 2;
+
+                if (op >= ref + len)
+                  {
+                    /* disjunct areas */
+                    memcpy (op, ref, len);
+                    op += len;
+                  }
+                else
+                  {
+                    /* overlapping, use octte by octte copying */
+                    do
+                      *op++ = *ref++;
+                    while (--len);
+                  }
+
+                break;
+
+              case 9: *op++ = *ref++;
+              case 8: *op++ = *ref++;
+              case 7: *op++ = *ref++;
+              case 6: *op++ = *ref++;
+              case 5: *op++ = *ref++;
+              case 4: *op++ = *ref++;
+              case 3: *op++ = *ref++;
+              case 2: *op++ = *ref++;
+              case 1: *op++ = *ref++;
+              case 0: *op++ = *ref++; /* two octets more */
+                      *op++ = *ref++;
+            }
+#endif
+        }
+    }
+  while (ip < in_end);
+
+  return op - (u8 *)out_data;
+}
+
 static int decompress_texture_thread(AVCodecContext *avctx, void *arg,
                                      int block_nb, int thread_nb)
 {
     DXVContext *ctx = avctx->priv_data;
     AVFrame *frame = arg;
-    int x = (TEXTURE_BLOCK_W * block_nb) % avctx->coded_width;
-    int y = TEXTURE_BLOCK_H * (TEXTURE_BLOCK_W * block_nb / avctx->coded_width);
+    int x = (TEXTURE_BLOCK_W * block_nb) % (avctx->coded_width);
+    int y = TEXTURE_BLOCK_H * (TEXTURE_BLOCK_W * block_nb / (avctx->coded_width));
     uint8_t *p = frame->data[0] + x * 4 + y * frame->linesize[0];
     const uint8_t *d = ctx->tex_data + block_nb * ctx->tex_step;
 
@@ -65,8 +211,8 @@ static void decompress_texture(AVCodecContext *avctx, AVFrame *frame)
     int x, y;
     uint8_t *src = ctx->tex_data;
 
-    for (y = 0; y < avctx->height; y += 4) {
-        for (x = 0; x < avctx->width; x += 4) {
+    for (y = 0; y < FFALIGN(avctx->coded_height, 16); y += 4) {
+        for (x = 0; x < FFALIGN(avctx->coded_width, 16); x += 4) {
             uint8_t *p = frame->data[0] + x * 4 + y * frame->linesize[0];
             int step = ctx->tex_funct(p, frame->linesize[0], src);
             src += step;
@@ -298,6 +444,21 @@ static int dxv_decompress_dxt5(AVCodecContext *avctx)
     return 0;
 }
 
+static int dxv_decompress_lzf(AVCodecContext *avctx)
+{
+    DXVContext *ctx = avctx->priv_data;
+    GetByteContext *gbc = &ctx->gbc;
+
+    int ret = lzf_decompress(gbc->buffer, bytestream2_get_bytes_left(gbc),
+                             ctx->tex_data, ctx->tex_size);
+    if (ret == 0) {
+        av_log(avctx, AV_LOG_ERROR, "lzf error %s\n.", strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
 static int dxv_decode(AVCodecContext *avctx, void *data,
                       int *got_frame, AVPacket *avpkt)
 {
@@ -324,14 +485,14 @@ static int dxv_decode(AVCodecContext *avctx, void *data,
         ctx->tex_rat   = 8;
         ctx->tex_step  = 8;
         decompress_tex = dxv_decompress_dxt1;
-        compression    = "DXT1";
+        av_log(avctx, AV_LOG_VERBOSE, "DXTR1 compression and DXT1 texture\n");
         break;
     case MKBETAG('D', 'X', 'T', '5'):
         ctx->tex_funct = ctx->texdsp.dxt5_block;
         ctx->tex_rat   = 4;
         ctx->tex_step  = 16;
         decompress_tex = dxv_decompress_dxt5;
-        compression    = "DXT5";
+        av_log(avctx, AV_LOG_VERBOSE, "DXTR5 compression and DXT5 texture\n");
         break;
     case MKBETAG('Y', 'C', 'G', '6'):
     case MKBETAG('Y', 'G', '1', '0'):
@@ -341,39 +502,33 @@ static int dxv_decode(AVCodecContext *avctx, void *data,
     default:
         size = tag & 0x00FFFFFF;
         old_type = tag >> 24;
-        av_log(avctx, AV_LOG_DEBUG, "%d %d\n", size, channels);
-        // = tag & 0xFF;
-        switch (old_type) {
-            // lz4decompress
-            // lz4decompress
-            // lzfdecompress
-            // dxtrdxt1
-            // dxtrdxt5
-            // dxtrycog6
-            // dxtryacoc10
-            // none
-            case 0x23:
-            case 0x63:
-            default:
-                av_log(avctx, AV_LOG_ERROR,
-                       "Unsupported tag header %s (0x%08X).\n", buf, tag);
-                return AVERROR_INVALIDDATA;
+        channels = old_type & 0x0F;
+        if (old_type & 0x40) {
+            av_log(avctx, AV_LOG_VERBOSE, "LZF compression and DXT5 texture\n");
+            ctx->tex_funct = ctx->texdsp.dxt5_block;
+            ctx->tex_step  = 16;
+        } else {
+            av_log(avctx, AV_LOG_VERBOSE, "LZF compression and DXT1 texture\n");
+            ctx->tex_funct = ctx->texdsp.dxt1_block;
+            ctx->tex_step  = 8;
         }
+        decompress_tex = dxv_decompress_lzf;
+        ctx->tex_rat = 1;
+        break;
     }
-    av_log(avctx, AV_LOG_VERBOSE, "%s texture\n", compression);
 
+    /* Version 3 has a 12 byte header instead of a 4 byte one. */
     if (!old_type) {
         channels = bytestream2_get_byte(gbc);
         bytestream2_skip(gbc, 3); // unknown
-        av_log(avctx, AV_LOG_DEBUG, "%d channels\n", channels);
-
         size = bytestream2_get_le32(gbc);
     }
     if (size > bytestream2_get_bytes_left(gbc)) {
-        av_log(avctx, AV_LOG_ERROR, "Incomplete file (%u > %u)\n.",
+        av_log(avctx, AV_LOG_ERROR, "Incomplete or invalid file (%u > %u)\n.",
                size, bytestream2_get_bytes_left(gbc));
         return AVERROR_INVALIDDATA;
     }
+    av_log(avctx, AV_LOG_TRACE, "%d channels advertised\n", channels);
 
     ret = ff_get_buffer(avctx, frame, 0);
     if (ret < 0)
@@ -384,7 +539,9 @@ static int dxv_decode(AVCodecContext *avctx, void *data,
     if (ret < 0)
         return ret;
 
-    decompress_tex(avctx);
+    ret = decompress_tex(avctx);
+    if (ret < 0)
+        return ret;
 
 #if 0
     /* Use the decompress function on the texture, one block per thread. */
@@ -413,9 +570,9 @@ static int dxv_init(AVCodecContext *avctx)
         return ret;
     }
 
-    /* Since codec is based on 4x4 blocks, size is aligned to 4. */
-    avctx->coded_width  = FFALIGN(avctx->width,  TEXTURE_BLOCK_W);
-    avctx->coded_height = FFALIGN(avctx->height, TEXTURE_BLOCK_H);
+    /* Codec requires 16x16 alignment. */
+    avctx->coded_width  = FFALIGN(avctx->width,  16);
+    avctx->coded_height = FFALIGN(avctx->height, 16);
 
     ff_texturedsp_init(&ctx->texdsp);
     avctx->pix_fmt = AV_PIX_FMT_RGBA;
