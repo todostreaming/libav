07477668867b3447acfe855d17353af5864b4cd6
Vittorio Giovara <vittorio.giovara@gmail.com>
2015-07-06 13:03:41 +0100

    4luca

--- libavcodec/dxv.c
+++ libavcodec/dxv.c
@@ -39,156 +39,65 @@ typedef struct DXVContext {
     uint8_t *tex_data;  // Compressed texture
     int tex_rat;        // Compression ratio
     int tex_step;       // Distance between blocks
-    int tex_size;       // Texture size
+    int64_t tex_size;   // Texture size
 
     /* Pointer to the selected decompression function */
     int (*tex_funct)(uint8_t *dst, ptrdiff_t stride, const uint8_t *block);
 } DXVContext;
 
 
-#if USE_REP_MOVSB /* small win on amd, big loss on intel */
-#if (__i386 || __amd64) && __GNUC__ >= 3
-# define lzf_movsb(dst, src, len)                \
-   asm ("rep movsb"                              \
-        : "=D" (dst), "=S" (src), "=c" (len)     \
-        :  "0" (dst),  "1" (src),  "2" (len));
-#endif
-#endif
+#define LZF_LITERAL_MAX (1 << 5)
+#define LZF_LONG_BACKREF 7
 
-# include <errno.h>
-# define SET_ERRNO(n) errno = (n)
-# define CHECK_INPUT 1
-
-#define u8 uint8_t
-static unsigned int 
-lzf_decompress (const void *const in_data,  unsigned int in_len,
-                void             *out_data, unsigned int out_len)
+static int ff_lzf_uncompress(GetByteContext *gb, uint8_t **buf, int64_t *size)
 {
-  u8 const *ip = (const u8 *)in_data;
-  u8       *op = (u8 *)out_data;
-  u8 const *const in_end  = ip + in_len;
-  u8       *const out_end = op + out_len;
+    int ret     = 0;
+    uint8_t *p  = *buf;
+    int64_t len = 0;
 
-  do
-    {
-      unsigned int ctrl = *ip++;
+    while (bytestream2_get_bytes_left(gb) > 2) {
+        uint8_t s = bytestream2_get_byte(gb);
 
-      if (ctrl < (1 << 5)) /* literal run */
-        {
-          ctrl++;
-
-          if (op + ctrl > out_end)
-            {
-              SET_ERRNO (E2BIG);
-              return 0;
+        if (s < LZF_LITERAL_MAX) {
+            if (s > *size - len) {
+                *size += *size / 2;
+                ret = av_reallocp(buf, *size);
+                if (ret < 0)
+                    return ret;
             }
 
-#if CHECK_INPUT
-          if (ip + ctrl > in_end)
-            {
-              SET_ERRNO (EINVAL);
-              return 0;
-            }
-#endif
+            bytestream2_get_buffer(gb, p, s);
+            p   += s;
+            len += s;
+        } else {
+            int l   = 2 + (s >> 5);
+            int off = ((s & 0x1f) << 8) + 1;
 
-#ifdef lzf_movsb
-          lzf_movsb (op, ip, ctrl);
-#else
-          switch (ctrl)
-            {
-              case 32: *op++ = *ip++; case 31: *op++ = *ip++; case 30: *op++ = *ip++; case 29: *op++ = *ip++;
-              case 28: *op++ = *ip++; case 27: *op++ = *ip++; case 26: *op++ = *ip++; case 25: *op++ = *ip++;
-              case 24: *op++ = *ip++; case 23: *op++ = *ip++; case 22: *op++ = *ip++; case 21: *op++ = *ip++;
-              case 20: *op++ = *ip++; case 19: *op++ = *ip++; case 18: *op++ = *ip++; case 17: *op++ = *ip++;
-              case 16: *op++ = *ip++; case 15: *op++ = *ip++; case 14: *op++ = *ip++; case 13: *op++ = *ip++;
-              case 12: *op++ = *ip++; case 11: *op++ = *ip++; case 10: *op++ = *ip++; case  9: *op++ = *ip++;
-              case  8: *op++ = *ip++; case  7: *op++ = *ip++; case  6: *op++ = *ip++; case  5: *op++ = *ip++;
-              case  4: *op++ = *ip++; case  3: *op++ = *ip++; case  2: *op++ = *ip++; case  1: *op++ = *ip++;
-            }
-#endif
-        }
-      else /* back reference */
-        {
-          unsigned int len = ctrl >> 5;
+            if (l == LZF_LONG_BACKREF)
+                l += bytestream2_get_byte(gb);
 
-          u8 *ref = op - ((ctrl & 0x1f) << 8) - 1;
+            off += bytestream2_get_byte(gb);
 
-#if CHECK_INPUT
-          if (ip >= in_end)
-            {
-              SET_ERRNO (EINVAL);
-              return 0;
-            }
-#endif
-          if (len == 7)
-            {
-              len += *ip++;
-#if CHECK_INPUT
-              if (ip >= in_end)
-                {
-                  SET_ERRNO (EINVAL);
-                  return 0;
-                }
-#endif
+            if (off > p - *buf)
+                return AVERROR_INVALIDDATA;
+
+            if (l > *size - len) {
+                *size += *size / 2;
+                ret = av_reallocp(buf, *size);
+                if (ret < 0)
+                    return ret;
             }
 
-          ref -= *ip++;
+            av_memcpy_backptr(p, off, l);
 
-          if (op + len + 2 > out_end)
-            {
-              SET_ERRNO (E2BIG);
-              return 0;
-            }
-
-          if (ref < (u8 *)out_data)
-            {
-              SET_ERRNO (EINVAL);
-              return 0;
-            }
-
-#ifdef lzf_movsb
-          len += 2;
-          lzf_movsb (op, ref, len);
-#else
-          switch (len)
-            {
-              default:
-                len += 2;
-
-                if (op >= ref + len)
-                  {
-                    /* disjunct areas */
-                    memcpy (op, ref, len);
-                    op += len;
-                  }
-                else
-                  {
-                    /* overlapping, use octte by octte copying */
-                    do
-                      *op++ = *ref++;
-                    while (--len);
-                  }
-
-                break;
-
-              case 9: *op++ = *ref++;
-              case 8: *op++ = *ref++;
-              case 7: *op++ = *ref++;
-              case 6: *op++ = *ref++;
-              case 5: *op++ = *ref++;
-              case 4: *op++ = *ref++;
-              case 3: *op++ = *ref++;
-              case 2: *op++ = *ref++;
-              case 1: *op++ = *ref++;
-              case 0: *op++ = *ref++; /* two octets more */
-                      *op++ = *ref++;
-            }
-#endif
+            p   += l;
+            len += l;
         }
     }
-  while (ip < in_end);
 
-  return op - (u8 *)out_data;
+    *size = len;
+
+    return 0;
 }
 
 static int decompress_texture_thread(AVCodecContext *avctx, void *arg,
@@ -449,8 +358,7 @@ static int dxv_decompress_lzf(AVCodecContext *avctx)
     DXVContext *ctx = avctx->priv_data;
     GetByteContext *gbc = &ctx->gbc;
 
-    int ret = lzf_decompress(gbc->buffer, bytestream2_get_bytes_left(gbc),
-                             ctx->tex_data, ctx->tex_size);
+    int ret = ff_lzf_uncompress(gbc, &ctx->tex_data, &ctx->tex_size);
     if (ret == 0) {
         av_log(avctx, AV_LOG_ERROR, "lzf error %s\n.", strerror(errno));
         return -1;
