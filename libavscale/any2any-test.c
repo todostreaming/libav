eb5e2e171a878a1216084a39ee5a6e8bec63e1e8
Luca Barbato <lu_zero@gentoo.org>
2016-04-01 04:58:31 +0200

    avscale: Update tests to the amended api

--- libavscale/any2any-test.c
+++ libavscale/any2any-test.c
@@ -100,7 +100,7 @@ int main(int argc, char **argv)
     if (!avsctx)
         goto end;
 
-    ret = avscale_convert_frame(avsctx, src, dst);
+    ret = avscale_convert_frame(avsctx, dst, src);
     if (ret < 0) {
         printf("Failed\n");
         goto end;
