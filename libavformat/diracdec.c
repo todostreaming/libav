c3a93f05b82978da7bc8185e36cb547933afd65c
Luca Barbato <lu_zero@gentoo.org>
2016-10-26 20:17:31 +0200

    vc2: Support non-seekable input

--- libavformat/diracdec.c
+++ libavformat/diracdec.c
@@ -22,12 +22,18 @@
 #include "libavutil/intreadwrite.h"
 
 #include "avformat.h"
+#include "avio_internal.h"
 #include "internal.h"
 
 #define VC2_SEQUENCE_HEADER  0x00
 #define VC2_END_OF_SEQUENCE  0x10
 #define VC2_HQ_PICTURE       0xE8
 #define VC2_HEADER_SIZE (4 + 1 + 4 + 4)
+
+typedef struct DiracContext {
+    AVIOContext *buf;
+} DiracContext;
+
 
 static int dirac_probe(AVProbeData *p)
 {
@@ -54,6 +60,9 @@ static int dirac_read_header(AVFormatContext *s)
 
 static int parse_header(AVFormatContext *s)
 {
+    DiracContext *drc = s->priv_data;
+    uint8_t *buf;
+
     uint32_t start_code = avio_rl32(s->pb);
     uint8_t  parse_code = avio_r8(s->pb);
     uint32_t next_off   = avio_rb32(s->pb);
@@ -77,19 +86,37 @@ static int parse_header(AVFormatContext *s)
            next_off,
            prev_off);
 
+    // TODO skip end of sequence
     if (parse_code == VC2_END_OF_SEQUENCE)
         return AVERROR_EOF;
 
-    avio_skip(s->pb, next_off - VC2_HEADER_SIZE);
+    // TODO make less wasteful
+    avio_wl32(drc->buf, start_code);
+    avio_w8(drc->buf, parse_code);
+    avio_wb32(drc->buf, next_off);
+    avio_wb32(drc->buf, prev_off);
+
+    buf = av_mallocz(next_off);
+
+    avio_read(s->pb, buf, next_off - VC2_HEADER_SIZE);
+
+    avio_write(drc->buf, buf, next_off - VC2_HEADER_SIZE);
+
+    av_free(buf);
 
     return parse_code;
 }
 
 static int dirac_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
-    int64_t pos = avio_tell(s->pb);
-    int64_t end;
+    DiracContext *drc = s->priv_data;
     int ret;
+
+    if (!drc->buf) {
+        ret = avio_open_dyn_buf(&drc->buf);
+        if (ret < 0)
+            return ret;
+    }
 
     // TODO resync
     while ((ret = parse_header(s)) != VC2_HQ_PICTURE) {
@@ -97,21 +124,34 @@ static int dirac_read_packet(AVFormatContext *s, AVPacket *pkt)
             return ret;
     }
 
-    end = avio_tell(s->pb);
+    pkt->size = avio_close_dyn_buf(drc->buf, &pkt->data);
+    drc->buf = NULL;
 
-    avio_seek(s->pb, pos, SEEK_SET);
+    if ((ret = av_packet_from_data(pkt, pkt->data, pkt->size)) < 0) {
+        av_freep(&pkt->data);
+        return ret;
+    }
 
-    av_log(s, AV_LOG_VERBOSE, "Packet out %"PRId64"\n", end - pos);
+    return pkt->size;
+}
 
-    return av_get_packet(s->pb, pkt, end - pos);
+static int dirac_read_close(AVFormatContext *s)
+{
+    DiracContext *drc = s->priv_data;
+
+    ffio_free_dyn_buf(&drc->buf);
+
+    return 0;
 }
 
 AVInputFormat ff_dirac_demuxer = {
     .name           = "dirac",
     .long_name      = NULL_IF_CONFIG_SMALL("DRC"),
+    .priv_data_size = sizeof(DiracContext),
     .read_probe     = dirac_probe,
     .read_header    = dirac_read_header,
     .read_packet    = dirac_read_packet,
+    .read_close     = dirac_read_close,
     .extensions     = "drc,vc2",
     .raw_codec_id   = AV_CODEC_ID_DIRAC,
 };
